3b477f71cb62f45831b16bc939c12e05bbf15573
Shivansh <ssgyanibaba@gmail.com>
2023-10-16 12:07:37 +0530

    'Phase2'

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 import logging
-from telegram import Update,ReplyKeyboardMarkup
+from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler,MessageHandler,filters
-import os
 import pandas as pd
-import re
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -17,22 +15,25 @@ logger=logging.getLogger(__name__)
 
 Token='6644344026:AAHmKqa6mubIGELIZ-7zlFWnjrf6NhCw1nw'
 
-all_topics=[['World', 'Nation', 'Entertainment'], ['Sports', 'Science', 'Health'],['Business', 'Technology']]
-
 
 li=[]
+i=1
+
+
+# # Use a pipeline as a high-level helper
+# from transformers import pipeline
+
+# pipe = pipeline("text-classification", model="unitary/toxic-bert")
 
 
 
 #handeling start command
 async def start(update:Update,context:ContextTypes.DEFAULT_TYPE):
     author=update.message.from_user.first_name
-    reply="Hi! 👋 {}\n\n This is Testing phase one😃".format(author)
+    reply="Hi! 👋 {}\n\n Testing phase 2😃".format(author)
     await context.bot.send_message(chat_id=update.effective_chat.id,text=reply)
-    # li=[]
     li.append(author)
     print(author)
-
 
 #handeling help command
 async def help(update:Update,context : ContextTypes.DEFAULT_TYPE):
@@ -40,38 +41,39 @@ async def help(update:Update,context : ContextTypes.DEFAULT_TYPE):
 
 
 
-#echoing message
-async def reply_text(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id,text=update.message.text)
+# message
+async def reply_text(update:Update,context : ContextTypes.DEFAULT_TYPE ):
+    global i
+    author=update.message.from_user.first_name
+    for entity in update.message.entities:
+        if entity['type'] == 'url':
+            reply="This is warning {}.  {} Sending links are not allowed".format(i,author)
+            if i==3:
+                await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+                await context.bot.send_message(chat_id=update.effective_chat.id,text="You voilated our rules")
+                await context.bot.ban_chat_member(chat_id=update.effective_chat.id, user_id=update.message.from_user.id)
+            else:
+                i=i+1
+                await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+                await context.bot.send_message(chat_id=update.effective_chat.id,text=reply)
 
-#echoing stickers
+# stickers
 async def echo_sticker(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_sticker(chat_id=update.effective_chat.id,sticker=update.message.sticker.file_id)
-
-
-#echoing audio
-async def echo_audio(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_audio(chat_id=update.effective_chat.id,audio=update.message.audio)
-
-#echoing video
-async def echo_video(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_video(chat_id=update.effective_chat.id,video=update.message.video.file_id)
-
-#echoing voice
-async def echo_voice(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_voice(chat_id=update.effective_chat.id,voice=update.message.voice.file_id)
-
-#echoing photo
-async def echo_photo(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_photo(chat_id=update.effective_chat.id,photo=update.message.photo[-1])
-
-#echoing video note
-async def echo_video_note(update:Update,context : ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_video_note(chat_id=update.effective_chat.id,video_note=update.message.video_note.file_id)
+    global i
+    author=update.message.from_user.first_name
+    reply="This is warning {}.  {} Sending stickers are not allowed".format(i,author)
+    if i==3:
+        await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+        await context.bot.send_message(chat_id=update.effective_chat.id,text="You voilated our rules")
+        await context.bot.ban_chat_member(chat_id=update.effective_chat.id, user_id=update.message.from_user.id)
+    else:
+        i=i+1
+        await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+        await context.bot.send_message(chat_id=update.effective_chat.id,text=reply)
 
 #For unknown commands
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="This command doesnot exist. Please try with valid commands")
 
 # Error handler
 
@@ -81,12 +83,9 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) :
     logger.error("Exception while handling an update:", exc_info=context.error)
 
 
-# def error(update:Update,context : ContextTypes.DEFAULT_TYPE):
-#     logger.error("Update caused error ")
-
 #Main Function
-
 def main():
+
     application = ApplicationBuilder().token(Token).build()
     
     #Making command handler for start
@@ -95,22 +94,10 @@ def main():
     application.add_handler(CommandHandler('help',help))
 
 
-
     #Making message handler
     application.add_handler(MessageHandler(filters.TEXT ,reply_text))
     #Making sticker handler 
     application.add_handler(MessageHandler(filters.Sticker.ALL,echo_sticker))
-    #Making audio handler 
-    application.add_handler(MessageHandler(filters.AUDIO,echo_audio))
-    #Making video handler
-    application.add_handler(MessageHandler(filters.VIDEO,echo_video))
-    #Making voice handler
-    application.add_handler(MessageHandler(filters.VOICE,echo_voice))
-    #Making photo handler
-    application.add_handler(MessageHandler(filters.PHOTO,echo_photo))
-    #Making video note handler
-    application.add_handler(MessageHandler(filters.VIDEO_NOTE,echo_video_note))
-
     #unknown command handler
     application.add_handler(MessageHandler(filters.COMMAND, unknown))
 
