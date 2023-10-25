39110f93439729033d4302b4f37ecee0c6940a14
Shivansh <ssgyanibaba@gmail.com>
2023-10-25 23:34:12 +0530

    Done_Multilinugal

--- vardaan69.py
+++ vardaan69.py
@@ -1,5 +1,3 @@
-import librosa
-import numpy as np
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 import pymongo
@@ -7,6 +5,8 @@ from logger import logging
 import os
 import requests
 from transformers import pipeline
+from pydub import AudioSegment
+import speech_recognition as sr
 
 ####################################################################################################
 
@@ -32,6 +32,20 @@ except Exception as e:
 # Initialising Everything
 
 Token = '6644344026:AAHmKqa6mubIGELIZ-7zlFWnjrf6NhCw1nw'
+
+
+async def reply_custom_text(update: Update, context: ContextTypes.DEFAULT_TYPE, text):
+    author = update.message.from_user.first_name
+    try:
+        pipe = pipeline("text-classification", model="unitary/toxic-bert")
+        logging.info(f'{pipe(text)[0]} by {author}')
+        print(pipe(text)[0])
+        if pipe(text)[0]['score'] > 0.85:
+            reply = "This is warning.  {} please don't use abusive words!".format(author)
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text=reply)
+    except Exception as e:
+        logging.error(f'Error occurred while handling the text by user {author}', exc_info=context.error)
 
 
 # handeling start command
@@ -180,25 +194,57 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         file_id = update.message.voice.file_id
         voice_note_file = await context.bot.get_file(file_id)
+        # voice_note_file.download(os.path.join(os.getcwd(),'voice_message.ogg'))
         print(voice_note_file.file_path)
         logging.info(f'Voice send by {author} voice file is {voice_note_file.file_path}')
 
         audio_url = voice_note_file.file_path
         response = requests.get(audio_url)
+
         if response.status_code == 200:
-            with open('temp_audio.mp3', 'wb') as temp_file:
-                temp_file.write(response.content)
+            with open('voice_message.ogg', 'wb') as f:
+                f.write(response.content)
+            ogg_file = os.path.join(os.getcwd(), 'voice_message.ogg')
+            wav_file = os.path.join(os.getcwd(), 'temp_voice.wav')
+            print(ogg_file)
+            print(wav_file)
+            oga_file = AudioSegment.from_file(ogg_file, format="ogg")
 
-            audio_data, sample_rate = librosa.load('temp_audio.mp3', sr=None)
+            # Export it as a .wav file
+            oga_file.export(wav_file, format="wav")
+            # audio_data, sample_rate = librosa.load('temp_audio.mp3', sr=None)
 
+            print("Exported")
+            # np.save('audio_array.npy', audio_data)
+            recognizer = sr.Recognizer()
+            audio_file = wav_file
+            with sr.AudioFile(audio_file) as source:
+                audio_data = recognizer.record(source)
 
-            print("Shape of the audio data array:", audio_data.shape)
-            np.save('audio_array.npy', audio_data)
-            pipe = pipeline("automatic-speech-recognition", model="openai/whisper-medium")
-            print(pipe(audio_data))
-            os.remove('temp_audio.mp3')
+            try:
+                reply = recognizer.recognize_google(audio_data)  # Use Google Web Speech API
+                print(reply)
+                try:
+                    pipe = pipeline("text-classification", model="unitary/toxic-bert")
+                    logging.info(f'{pipe(reply)[0]} by {author}')
+                    print(pipe(reply)[0])
+                    if pipe(reply)[0]['score'] > 0.85:
+                        reply = "This is warning.  {} you used--> {} words which are offencive!".format(author, reply)
+                        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                                       text=reply)
+                    else:
+                        reply = "You said --> {}".format( reply)
+                        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                                       text=reply)
+                except Exception as e:
+                    logging.error(f'Error occurred while handling the text by user {author}', exc_info=context.error)
+            except sr.UnknownValueError:
+                print("Google Web Speech Recognition could not understand voice")
+
+            os.remove(ogg_file)
+            os.remove(wav_file)
         else:
-            print(f"Failed to download the audio. Status code: {response.status_code}")
+            logging.info(f"Failed to download the audio. Status code: {response.status_code}")
 
     except Exception as e:
         logging.error(f'Error while handling voice by {author} {e}', exc_info=context.error)
@@ -216,16 +262,43 @@ async def handle_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
         response = requests.get(audio_url, stream=True)
 
         if response.status_code == 200:
-            with open('temp_audio.mp3', 'wb') as temp_file:
-                temp_file.write(response.content)
+            with open('audio_message.mp3', 'wb') as f:
+                f.write(response.content)
+            mp3_file_path = os.path.join(os.getcwd(), 'audio_message.mp3')
+            wav_file = os.path.join(os.getcwd(), 'temp_audio.wav')
+            print(mp3_file_path)
+            print(wav_file)
+            mp3_file = AudioSegment.from_file(mp3_file_path, format="mp3")
+            mp3_file.export(wav_file, format="wav")
+            print("Exported")
+            recognizer = sr.Recognizer()
+            audio_file = wav_file
+            with sr.AudioFile(audio_file) as source:
+                audio_data = recognizer.record(source)
 
-            audio_data, sample_rate = librosa.load('temp_audio.mp3', sr=None)
-            np.save('audio_array.npy', audio_data)
-            print("Shape of the audio data array:", audio_data.shape)
-
-            pipe = pipeline("automatic-speech-recognition", model="openai/whisper-medium")
-            print(pipe(audio_data))
-            os.remove('temp_audio.mp3')
+            try:
+                reply = recognizer.recognize_google(audio_data)  # Use Google Web Speech API
+                print(reply)
+                try:
+                    pipe = pipeline("text-classification", model="unitary/toxic-bert")
+                    logging.info(f'{pipe(reply)[0]} by {author}')
+                    print(pipe(reply)[0])
+                    if pipe(reply)[0]['score'] > 0.85:
+                        reply = "This is warning.  {} you used --> {} words which are offencive!".format(author, reply)
+                        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                                       text=reply)
+                    else:
+                        reply = "You said --> {}".format(reply)
+                        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                                       text=reply)
+                except Exception as e:
+                    logging.error(f'Error occurred while handling the text by user {author}', exc_info=context.error)
+            except sr.UnknownValueError:
+                print("Google Web Speech Recognition could not understand audio")
+            os.remove(mp3_file_path)
+            os.remove(wav_file)
+            await context.bot.send_message(chat_id=update.effective_chat.id,
+                                           text=f"Your audio is {reply}")
         else:
             logging.info(f"Failed to download the audio. Status code: {response.status_code}")
 
@@ -253,6 +326,8 @@ def main():
     application.add_handler(CommandHandler('start', start))
     # Making command handler for help
     application.add_handler(CommandHandler('help', help))
+    # Making report handler for report
+    # application.add_handler(CommandHandler('report', report))
 
     # Making message handler
     application.add_handler(MessageHandler(filters.TEXT, reply_text))
--- x.py
+++ x.py
@@ -29,23 +29,28 @@
 # sd.play(data, sample_rate)
 # sd.wait()
 # import soundfile as sf
+import os
 
+import torch
 ###To play the audio
 # # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
-import sounddevice as sd
-import numpy as np
+# import sounddevice as sd
+# import numpy as np
+#
+# # Replace 'audio_data.npy' with the path to your .npy file
+# audio_file = r'C:\Users\Dell\Desktop\PROJECT\HTM\audio_array.npy'
+#
+# # Load the audio data from the .npy file
+# audio_data = np.load(audio_file)
+#
+# # Play the audio using sounddevice
+# sd.play(audio_data, samplerate=44100)  # Replace 44100 with the appropriate sample rate
+#
+# # Wait for the audio to finish playing
+# sd.wait()
 
-# Replace 'audio_data.npy' with the path to your .npy file
-audio_file = r'C:\Users\Dell\Desktop\PROJECT\HTM\audio_array.npy'
 
-# Load the audio data from the .npy file
-audio_data = np.load(audio_file)
 
-# Play the audio using sounddevice
-sd.play(audio_data, samplerate=44100)  # Replace 44100 with the appropriate sample rate
-
-# Wait for the audio to finish playing
-sd.wait()
 
 # import librosa
 #
@@ -60,4 +65,82 @@ sd.wait()
 # # Replace 'input.mp3' with the path to your input MP3 file
 
 
+
+
+
+# import librosa
+# from transformers import pipeline
+# import soundfile as sf
+#
+# # Specify the path to your audio file
+# audio_file = r"C:\Users\Dell\Desktop\Recording (10).wav"
+#
+# # Read the audio file using librosa
+# audio, sample_rate = librosa.load(audio_file, sr=None)
+#
+# print(audio.size)
+# pipe = pipeline("automatic-speech-recognition", model="openai/whisper-medium")
+# print(pipe(audio))
+
+
+
+
+# from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+#
+# # Load the processor and model
+# processor = AutoProcessor.from_pretrained("openai/whisper-large-v2")
+# model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-large-v2")
+#
+# # Load and preprocess your audio data (replace 'audio.wav' with your audio file)
+# # Ensure it matches the model's requirements for sampling rate, etc.
+#
+# # Tokenize the audio data using the processor
+# inputs = processor(r"C:\Users\Dell\Desktop\temp_audio.wav", return_tensors="pt", padding="longest")
+#
+# # Perform inference to convert audio into text
+# with torch.no_grad():
+#     generated_ids = model.generate(inputs["input_features"])
+
+# Decode the generated text
+# transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+#
+# # Print the transcription
+# print(transcription)
+
+import speech_recognition as sr
+
+from pydub import AudioSegment
+
+
+oga_file = r'C:\Users\Dell\Desktop\PROJECT\HTM\audio_message.mp3'
+wav_file = os.path.join(os.getcwd(),'temp_audioxxx.wav')
+# # Load the .oga file
+# oga_file = AudioSegment.from_file(oga_file, format="ogg")
+#
+# # Export it as a .wav file
+# oga_file.export(wav_file, format="wav")
+
+
+
+
+mp3_file = AudioSegment.from_file(oga_file, format="mp3")
+
+# Export it as a WAV file
+mp3_file.export(wav_file, format="wav")
+
+
+# # import speech_recognition as sr
+# recognizer = sr.Recognizer()
+#
+# audio_file = os.path.join(os.getcwd(),'temp_audioxxx.wav')
+# with sr.AudioFile(audio_file) as source:
+#     audio_data = recognizer.record(source)
+#
+# try:
+#     text = recognizer.recognize_google(audio_data)  # Use Google Web Speech API
+#     print("Google Web Speech Recognition: " + text)
+# except sr.UnknownValueError:
+#     print("Google Web Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Google Web Speech Recognition; {0}".format(e))
 
