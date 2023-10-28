2b3769632c4d22f75111606592bef5bb2e5c8be5
Shivansh Singh <122978299+Shiv-Expert2503@users.noreply.github.com>
2023-10-28 09:28:47 +0530

    'Added_/translate'

--- HTM_FINAL.py
+++ HTM_FINAL.py
@@ -6,7 +6,8 @@ import speech_recognition as sr
 from googletrans import Translator
 from pydub import AudioSegment
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackContext, \
+    ConversationHandler
 from transformers import pipeline
 
 from logger import logging
@@ -37,27 +38,13 @@ except Exception as e:
 Token = '6644344026:AAHmKqa6mubIGELIZ-7zlFWnjrf6NhCw1nw'
 
 
-async def reply_custom_text(update: Update, context: ContextTypes.DEFAULT_TYPE, text):
-    author = update.message.from_user.first_name
-    try:
-        pipe = pipeline("text-classification", model="unitary/toxic-bert")
-        logging.info(f'{pipe(text)[0]} by {author}')
-        print(pipe(text)[0])
-        if pipe(text)[0]['score'] > 0.85:
-            reply = "This is warning.  {} please don't use abusive words!".format(author)
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text=reply)
-    except Exception as e:
-        logging.error(f'Error occurred while handling the text by user {author}', exc_info=context.error)
-
-
-# handeling start command
+# handling start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     author = update.message.from_user.first_name
     logging.info(f'Bot Started By user {author}')
     database[colletion_name].insert_one({author: "hello there"})
 
-    reply = "Hi! 👋 {}\n\n Testing phase 2😃".format(author)
+    reply = "Hi! 👋 {}\n\n Testing Final Phase😃".format(author)
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text=reply)
     li.append(author)
@@ -70,101 +57,41 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info(f"Help command activated by user {author}")
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Can't help at this moment kindley contact Shivansh personally!")
+        text="Can't help at this moment kindly contact Shivansh personally!")
 
 
-# # Handeling Translate command
-# async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await context.bot.send_message(chat_id=update.effective_chat.id, text="Enter the text")
-#     try:
-#         author = update.message.from_user.first_name
-#         logging.info(f"Translate command activated by {author}")
-#         translator = Translator()
-#         text_to_translate = update.message.text
-#         detected_language = translator.detect(text_to_translate).lang
-#         target_language = 'hi'
-#         translated_text = translator.translate(text_to_translate, dest=target_language)
-#         print(f"Original Text: {text_to_translate} (Detected Language: {detected_language})")
-#         print(f"Translation to {target_language}: {translated_text.text}")
-#         pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-hi-en")
-#         print(pipe(translated_text.text)[0]['translation_text'])
-#         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{translated_text.text}")
-#     except Exception as e:
-#         logging.error(f"Error translating {update.message.text}", exc_info=context.error)
-#
-#
-# # Handeling Voice Translate command
-# async def translate_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     author = update.message.from_user.first_name
-#     try:
-#         # Getting File id
-#         file_id = update.message.voice.file_id
-#         voice_note_file = await context.bot.get_file(file_id)
-#         print(voice_note_file.file_path)
-#         logging.info(f'Voice send by {author} voice file is {voice_note_file.file_path}')
-#
-#         audio_url = voice_note_file.file_path
-#         response = requests.get(audio_url)
-#
-#         # Fetching data from the url
-#
-#
-#         if response.status_code == 200:
-#             with open('voice_message.ogg', 'wb') as f:
-#                 f.write(response.content)
-#             ogg_file = os.path.join(os.getcwd(), 'voice_message.ogg')
-#             wav_file = os.path.join(os.getcwd(), 'temp_voice.wav')
-#             print(ogg_file)
-#             print(wav_file)
-#             oga_file = AudioSegment.from_file(ogg_file, format="ogg")
-#
-#             # Export it as a .wav file
-#             oga_file.export(wav_file, format="wav")
-#             print("Exported")
-#             recognizer = sr.Recognizer()
-#             audio_file = wav_file
-#             with sr.AudioFile(audio_file) as source:
-#                 audio_data = recognizer.record(source)
-#
-#             #Trying audio analysis
-#             try:
-#                 reply = recognizer.recognize_google(audio_data)
-#                 print(reply)
-#                 try:
-#                     translator = Translator()
-#                     text_to_translate = recognizer.recognize_google(audio_data)
-#                     detected_language = translator.detect(text_to_translate).lang
-#                     target_language = 'hi'
-#                     translated_text = translator.translate(text_to_translate, dest=target_language)
-#                     print(f"Original Text: {text_to_translate} (Detected Language: {detected_language})")
-#                     print(f"Translation to {target_language}: {translated_text.text}")
-#                     pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-hi-en")
-#                     print(pipe(translated_text.text)[0]['translation_text'])
-#                     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"You said ==>{reply}...And here is the translated voice ==>{translated_text.text}")
-#                 except Exception as e:
-#                     logging.error(f"Error translating {update.message.text}", exc_info=context.error)
-#             except sr.UnknownValueError:
-#                 print("Google Web Speech Recognition could not understand voice")
-#
-#             os.remove(ogg_file)
-#             os.remove(wav_file)
-#         else:
-#             logging.info(f"Failed to download the audio. Status code: {response.status_code}")
-#
-#     except Exception as e:
-#         logging.error(f'Error while handling voice by {author} {e}', exc_info=context.error)
-#
-# # Handeling Audio Translate command
-# async def translate_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     try:
-#         pass
-#     except Exception as e:
-#         logging.error(f"Error translating {update.message.text}", exc_info=context.error)
+# Handling translate command
+async def translate(update: Update, context: CallbackContext):
+    await update.message.reply_text("Please enter the text:")
+    return "GET_TEXT"
 
 
-# Handeling Report
-# async def report(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     #To be continued by vardaan
+async def get_trans_lang(update: Update, context: CallbackContext):
+    try:
+        context.user_data['text'] = str(update.message.text)
+        await update.message.reply_text(
+            "Please enter the language you want it to get translated(Please see languages available and type their "
+            "short forms) :")
+        return "GET_LANG"
+    except ValueError:
+        await update.message.reply_text("Invalid Language")
+        return "GET_TEXT"
+
+
+async def get_text(update: Update, context: CallbackContext):
+    try:
+        target_language = update.message.text
+        translator = Translator()
+        text_to_translate = context.user_data['text']
+        detected_language = translator.detect(text_to_translate).lang
+        print(target_language)
+        translated_text = translator.translate(text_to_translate, dest=target_language)
+        print(f"Original Text: {text_to_translate} (Detected Language: {detected_language})")
+        print(f"Translation to {target_language}: {translated_text.text}")
+        await update.message.reply_text(f"The translation is {translated_text.text}")
+    except ValueError:
+        await update.message.reply_text("Invalid inputs.")
+    return ConversationHandler.END
 
 
 # message
@@ -313,7 +240,6 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         # Fetching data from the url
 
-
         if response.status_code == 200:
             with open('voice_message.ogg', 'wb') as f:
                 f.write(response.content)
@@ -331,7 +257,7 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
             with sr.AudioFile(audio_file) as source:
                 audio_data = recognizer.record(source)
 
-            #Trying audio analysis
+            # Trying audio analysis
             try:
                 reply = recognizer.recognize_google(audio_data)
                 print(reply)
@@ -437,14 +363,26 @@ def main():
     application.add_handler(CommandHandler('start', start))
     # Making command handler for help
     application.add_handler(CommandHandler('help', help))
-    # # Making command handler for translate
-    # application.add_handler(CommandHandler('translate', translate))
-    # # Making report handler for report
-    # # application.add_handler(CommandHandler('report', report))
-    # # Making command handler for translate_voice
-    # application.add_handler(CommandHandler('translate_voice', translate_voice))
-    # # Making command handler for translate_audio
-    # application.add_handler(CommandHandler('translate_audio', translate_audio))
+
+    # Making command handler for translate
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('translate', translate)],
+        states={
+            "GET_TEXT": [MessageHandler(filters.TEXT & ~filters.COMMAND, get_trans_lang)],
+            "GET_LANG": [MessageHandler(filters.TEXT & ~filters.COMMAND, get_text)]
+        },
+        fallbacks=[],
+    )
+
+    application.add_handler(conversation_handler)
+
+
+
+    #To be completed by VARDAAN<=============================================================================
+    # application.add_handler(CommandHandler('report',report))
+
+    
+
     # Making message handler
     application.add_handler(MessageHandler(filters.TEXT, reply_text))
     # Making sticker handler
